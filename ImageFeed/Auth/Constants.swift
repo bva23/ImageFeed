4d521cc14c6429c5b636e4f5835f4d33e7ed10b8
bva23 <bva.mos@yandex.ru>
2023-06-12 15:10:15 +0300

    Выполнена задача по спринту 10.

---
+++ ImageFeed/Auth/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Владимир Богомолов on 29.05.2023.
+//
+
+import Foundation
+
+let accessKey = "MnJNOUZX45XldCEjcxqqKhuVvQ64Hz0AUqO7s2uLy-c"
+let secretKey = "xIZySRjd5etR7dO8m8GaNZ1rgH3E9cab84UJvGAsEg4"
+let redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseUrl = URL(string: "https://api.unsplash.com")!
